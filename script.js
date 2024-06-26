1c247c0517dce4bfc5c84a2f66444f6548cf544c
lerenty <luke3cial@gmail.com>
2024-06-25 22:26:21 -0300

    r

--- script.js
+++ script.js
@@ -7,23 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
     botaoNao.addEventListener('mouseover', () => {
         const largura = window.innerWidth - botaoNao.clientWidth;
         const altura = window.innerHeight - botaoNao.clientHeight;
-        let novaPosicaoX = Math.random() * largura;
-        let novaPosicaoY = Math.random() * altura;
-
-        // Certifique-se de que o botão "Não" não se sobreponha ao botão "Sim"
-        const margemSegura = 50; // Distância mínima entre os botões
-
-        const posicaoSim = botaoSim.getBoundingClientRect();
-        const posicaoNao = botaoNao.getBoundingClientRect();
-
-        while (
-            novaPosicaoX > posicaoSim.left - margemSegura && novaPosicaoX < posicaoSim.right + margemSegura &&
-            novaPosicaoY > posicaoSim.top - margemSegura && novaPosicaoY < posicaoSim.bottom + margemSegura
-        ) {
-            novaPosicaoX = Math.random() * largura;
-            novaPosicaoY = Math.random() * altura;
-        }
-
+        const novaPosicaoX = Math.random() * largura;
+        const novaPosicaoY = Math.random() * altura;
         botaoNao.style.position = 'absolute';
         botaoNao.style.left = `${novaPosicaoX}px`;
         botaoNao.style.top = `${novaPosicaoY}px`;
